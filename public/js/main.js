20a7eea14f325f41b889ea0081874d62979022a8
Juanita Corbette <59467011+jcorbette@users.noreply.github.com>
2022-09-17 23:52:43 -0400

    Added updated public folder

---
+++ public/js/main.js
@@ -0,0 +1,24 @@
+const header = document.querySelector('header');
+window.onscroll = () => {
+    if (window.scrollY > 1) {
+        header.classList.add('header-scroll');
+    } else {
+        header.classList.remove('header-scroll');
+    }
+};
+
+
+
+
+function menuFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+    
+}
+  
+  
+function menuBtnFunction(x) {
+x.classList.toggle("change");    
+}
+  
+
+  
